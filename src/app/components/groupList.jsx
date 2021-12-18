de44cd5d8706abac73d59b611dc30cbdc8fd1141
Roman Kharitonov <thebestpapaintheworld@gmail.com>
2021-12-18 23:20:12 +0300

    5

---
+++ src/app/components/groupList.jsx
@@ -0,0 +1,43 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+const GroupList = ({
+	items,
+	valueProperty,
+	contentProperty,
+	onItemSelect,
+	selectedItem
+}) => {
+	return (
+		<ul className="list-group">
+			{Object.keys(items).map((key) => {
+				return (
+					<li
+						key={items[key][valueProperty]}
+						className={`list-group-item ${
+							items[key] === selectedItem ? "active" : ""
+						}`}
+						onClick={() => onItemSelect(items[key])}
+						role="button"
+					>
+						{items[key][contentProperty]}
+					</li>
+				);
+			})}
+		</ul>
+	);
+};
+
+GroupList.defaultProps = {
+	valueProperty: "_id",
+	contentProperty: "name"
+};
+GroupList.propTypes = {
+	items: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+	valueProperty: PropTypes.string.isRequired,
+	contentProperty: PropTypes.string.isRequired,
+	onItemSelect: PropTypes.func,
+	selectedItem: PropTypes.object
+};
+
+export default GroupList;
